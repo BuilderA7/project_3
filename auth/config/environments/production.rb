4deb48d88f2c97a26507fed673cfcaa942dc818b
BuilderA7 <atbisrael@gmail.com>
2018-09-06 21:23:10 -0500

    revision auth

--- auth/config/environments/production.rb
+++ auth/config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => 'atbisrael',
-   :password             => 'atbisrael2221',
+   :user_name            => '',
+   :password             => '',
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
